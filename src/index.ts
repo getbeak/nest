d97176e5e1aa5eff01146976ee91779b838e663a
Alexander Forbes-Reed <git@alx.red>
2020-12-13 23:25:17 +0000

    Basic routing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-	const queries = JSON.stringify(event.queryStringParameters);
+export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const url = event.requestContext.path;
+	const urlSafe = url.toLowerCase();
 
-	return {
-		statusCode: 200,
-		body: `Query params params: ${queries}`,
-	};
+	switch (true) {
+		case urlSafe.startsWith('/download/'): // /download/:platform/:file
+			return { statusCode: 200, body: 'download!' };
+
+		case urlSafe.startsWith('/latest/'): // /latest/:platform/:extension
+			return { statusCode: 200, body: 'latest!' };
+
+		case urlSafe.startsWith('/update/'): // /update/:platform/:extension/:version
+			return { statusCode: 200, body: 'update!' };
+
+		case urlSafe.startsWith('/releases/'): // /releases/:platform/:extension/:version
+			return { statusCode: 200, body: 'releases!' };
+
+		default:
+			return {
+				statusCode: 404,
+				body: 'This is not the request you are looking for. Visit https://getbeak.app! 🐦',
+			};
+	}
 };
