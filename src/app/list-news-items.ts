79135d4eda06c28b391da0a88447fe3e5f6c5381
Alexander Forbes-Reed <git@alex.forbes.red>
2021-07-05 17:42:01 -0400

    updated news item

--- src/app/list-news-items.ts
+++ src/app/list-news-items.ts
@@ -6,8 +6,8 @@ const newsItems: NewsItem[] = [{
 		code: 'generic_banner',
 		payload: {
 			emoji: '🐦',
-			title: 'Welcome to the Beak Beta',
-			body: 'blah blah',
+			title: 'Welcome to the Beak Beta!',
+			body: 'Thank you for being part of the Beak beta! We\'re still getting ready to take flight, so the journey ahead may not be smooth. When you encounter bugs or features misbehaving just let us know using the feedback form provided in the email, the button below below, or in the menu bar above. Enjoy Beak, and play nice!',
 			action: {
 				cta: 'Feedback',
 				url: 'https://www.notion.so/beakapp/8e3f72a1103548c7a149de1485effda9?v=33ae478ec0524a57bc2a9ae0421ed63a',
